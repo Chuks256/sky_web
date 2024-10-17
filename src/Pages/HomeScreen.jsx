ef445e05afe7a880569a8a317d99af9c7f346356
chuks <alexanderchuks310@gmail.com>
2024-10-17 16:30:14 +0100

    still working

--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import { RiNotification4Fill } from "react-icons/ri";
+import { PiStarFourFill } from "react-icons/pi";
+import { IoMdArrowDropdown } from "react-icons/io";
+import {useState} from "react"
 
 const Container = styled.div`
 display:flex;
@@ -8,24 +12,50 @@ flex-direction:column;
 
 const AppBarHeader= styled.div`
 position:absolute;
-background:var(--sky-preference-royal-color);
+background:var(--sky-preference-default-color);
 width:100%;
 top:0px;
 height:40%;
 left:0px;
 `;
-const AppBarItemsContainer=styled.div ``;
-const UserProfilePics=styled.div``;
-const UserPointBalance =styled.div``;
+const AppBarItemsContainer=styled.div `
+display:flex;
+padding:35px;
+align-items:center;
+justify-content:space-between
+`;
+const UserProfilePics=styled.div`
+padding:25px;
+background:grey;
+border-radius:100px;
+`;
+const UserPointBalance =styled.div`
+padding:10px;
+background:var(--sky-preference-default-shade);
+border-radius:100px;
+width:120px;
+display:flex;
+align-items:center;
+justify-content:space-between;
+font-size:14px;
+`;
 
 const HomeScreen=()=>{
+
+    const [skyPoint,setSkyPointBalance]=useState("20k Sky")
+
     return(
         <Container>
             {/* Header */}
             <AppBarHeader>
                 <AppBarItemsContainer>
                     <UserProfilePics></UserProfilePics>
-                    <UserPointBalance></UserPointBalance>
+                    <UserPointBalance>
+                    <PiStarFourFill  size={20}/>
+                    {skyPoint}
+                    <IoMdArrowDropdown size={30} />
+                    </UserPointBalance>
+                    <RiNotification4Fill size={30} />
                 </AppBarItemsContainer>
             </AppBarHeader>
 
