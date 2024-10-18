9cf2663e2cbde3e2fa85eb935f81fe2b5ef8c2e4
chuks <alexanderchuks310@gmail.com>
2024-10-18 04:55:17 +0100

    just worked on botton navigation

--- src/Components/BottomNavigation.jsx
+++ src/Components/BottomNavigation.jsx
@@ -15,18 +15,57 @@ z-index:1;
 display:flex;
 align-items:center;
 justify-content:center;
-background:red;
-height:70px;
+background:rgba(16,17,17,40%);
+backdrop-filter:blur(40px);
+height:80px;
 width:98%;
 `;
 
 
 const BottomNavigationContainer=styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+gap:5em;
 `;
+
+const HomeNavContainer = styled.div`
+gap:5px;
+display:flex;
+align-items:center;
+flex-direction:column;
+font-size:12px`;
+
+const SearchNavContainer = styled.div`
+gap:5px;
+display:flex;
+align-items:center;
+flex-direction:column;
+font-size:12px`;
+
+const TaskNavContainer = styled.div`
+gap:5px;
+display:flex;
+align-items:center;
+flex-direction:column;
+font-size:12px`;
+
+const FriendNavContainer = styled.div`
+gap:5px;
+display:flex;
+align-items:center;
+flex-direction:column;
+font-size:12px`;
 
 const BottomNavigation = ()=>{
     return(
         <Container>
+            <BottomNavigationContainer>
+                <HomeNavContainer><GoHomeFill size={25}/> Home</HomeNavContainer>
+                <SearchNavContainer><IoSearch size={25}/> Search</SearchNavContainer>
+                <TaskNavContainer><RiTaskLine size={25}/> Task</TaskNavContainer>
+                <FriendNavContainer><BsPeople size={25}/> Frens</FriendNavContainer>
+            </BottomNavigationContainer>
         </Container>
     )
 }
