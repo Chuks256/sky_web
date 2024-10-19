689f7c256b8d8688884a9aff01b9d27a515a6265
chuks <alexanderchuks310@gmail.com>
2024-10-19 07:36:34 +0100

    done

--- src/Components/Post.modal.jsx
+++ src/Components/Post.modal.jsx
@@ -13,7 +13,7 @@ const Container = styled.div`
 display:flex;
 z-index:1;
 flex-direction:column;
-gap:5px;
+gap:9px;
 align-items:center;
 `
 const ParentContainer=styled.div`
@@ -71,6 +71,92 @@ align-items:center;
 gap:10em;
 `;
 
+const MediaContent = styled.div`
+background:grey;
+padding:20px;
+border-radius:5px;
+width:180px;
+height:180px;
+background-position:center;
+background-repeat:no-repeat;
+background-size:cover;
+`;
+
+const MediaContainer = styled.div`
+ display: grid;
+  gap: 3px;
+
+  ${
+    ((props)=>{
+      if(props.children.length===3){
+        return`
+        grid-template-columns:1fr 1fr;
+        :nth-child(1){
+        width:70px;
+        height:70px;
+        grid-column:1;
+        grid-row:1;
+        }
+        :nth-child(2){
+        width:70px;
+        height:70px;
+        grid-column:2;
+        grid-row:1;
+        }
+        :nth-child(3){
+        width:70px;
+        height:70px;
+        grid-column:1/span 2;
+        grid-row:2;
+        width:180px;
+        }
+      `
+      }
+
+      if(props.children.length===2){
+        return `
+         grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: minimax(150px, 1fr);
+        :nth-child(1){
+        width:70px;
+        height:100px;
+        }
+        :nth-child(2){
+        height:100px;
+        width:70px;
+        }
+        `
+      }
+
+      
+      if(props.children.length===4){
+        return `
+         grid-template-columns: repeat(2, 1fr);
+      grid-auto-rows: minimax(150px, 1fr);
+        :nth-child(1){
+        width:70px;
+        height:70px;
+      }
+         :nth-child(2){
+        width:70px;
+        height:70px;
+      }
+         :nth-child(3){
+        width:70px;
+        height:70px;
+      }
+         :nth-child(4){
+        width:70px;
+        height:70px;
+      }
+        
+        `
+      }
+      
+      
+    })
+  }
+`;
 
 
 
@@ -93,10 +179,21 @@ const PostModal=(props)=>{
 </ProfileContainer>
  
     {/* post content section */}
-    <PostContent>{props.postcontent}
- </PostContent>
+    <PostContent>{props.postcontent}{props.postimage.length} </PostContent>
 
-
+    
+    {/* media content section */}
+    {props.postimage.length>0?(
+    <MediaContainer style={{display:"grid"}}>
+    {props.postimage.map((data)=>(
+     <MediaContent style={{backgroundImage:`url(${data.src})`}}></MediaContent>
+    ))} 
+   </MediaContainer>
+    ):(
+      <></>
+    )
+    }
+    
  {/* reaction section */}
  <ReactionContainer>
    <BiUpvote size={19}/>
--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -75,47 +75,85 @@ const HomeScreen=()=>{
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore "
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore ",
+            postimage:[
+                {
+                    src:"https://i.pinimg.com/control/564x/7e/1e/42/7e1e42f47a4af412bfce7eee1df29708.jpg"
+                }
+            ]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore"
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore",
+            postimage:[
+                {
+                    src:"https://i.pinimg.com/736x/20/7e/f0/207ef089a3b11f0e52d2a40c80c7fc68.jpg"
+                },
+                {
+                    src:"https://i.pinimg.com/control/564x/43/d9/5b/43d95b2312ff74f118368708ea3438a5.jpg"
+                }
+            ]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore"
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore",
+            postimage:[
+                {src:"https://i.pinimg.com/control/474x/75/83/9c/75839c1c207394c929306c04871626c6.jpg" 
+                },
+                {
+                    src:"https://i.pinimg.com/736x/a6/46/11/a6461171b3897ba244050f60c0b63c47.jpg"
+                },
+                {
+                     src:"https://i.pinimg.com/control/564x/03/7a/1e/037a1ee9a1b9d4222f02acf4aa267c8f.jpg",
+                },
+                {
+                    src:"https://i.pinimg.com/564x/c6/cd/c2/c6cdc285dc0ec9b47aed926de291e0a1.jpg"
+                }
+            ]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore"
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore",
+            postimage:[
+                {src:"https://i.pinimg.com/474x/90/77/25/907725c1a28c7258a4ff4b602af20c9e.jpg"},
+                    {src:"https://i.pinimg.com/control/474x/e4/90/4e/e4904ed0b889260f9f428fb7a4d48b07.jpg"},
+                    {src:"https://i.pinimg.com/736x/3e/e5/21/3ee5216cbcbb6c11ff3ad37d2bbe9e5a.jpg"},
+            ]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore"
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore",
+            postimage:[
+                {src:"https://i.pinimg.com/control/564x/8b/13/79/8b137979ab5e74ef71563ca5066f0e1e.jpg"}
+            ]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et"
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et",
+            postimage:[]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore "
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore ",
+            postimage:[]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore "
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore ",
+            postimage:[]
         },
         {
             username:"JaneTheBaddie",
             timestamp:`${new Date().getSeconds()} Sec ago`,
-            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore "
+            postcontent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore ",
+            postimage:[]
         }
     ]
 
@@ -141,7 +179,7 @@ const HomeScreen=()=>{
             <PostParentContainer>
                 {
                     dummyData.map((data)=>(
-                        <PostModal revealPost="flex" username={data.username} timestamp={data.timestamp} postcontent={data.postcontent} />
+                        <PostModal revealPost="flex" postimage={data.postimage} username={data.username} timestamp={data.timestamp} postcontent={data.postcontent} />
                     ))
                 }
            
